7636f230c3f9f73f4a638374fc3929f919154484
Shivam Shrivastava <19shivamshrivastava@gmail.com>
2018-03-20 12:58:32 +0530

    Update compliments_parts_day.js

--- tests/configs/modules/compliments/compliments_parts_day.js
+++ tests/configs/modules/compliments/compliments_parts_day.js
@@ -1,7 +1,4 @@
-/* Magic Mirror Test config for default compliments
- *
- * By Rodrigo Ramírez Norambuena https://rodrigoramirez.com
- * MIT Licensed.
+/* Smart Mirror Test config for default compliments
  */
 
 var config = {
