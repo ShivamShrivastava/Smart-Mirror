621c5674d5d6eb52a69f5161277cd2891fafdd0d
Shivam Shrivastava <19shivamshrivastava@gmail.com>
2018-03-20 12:38:18 +0530

    Update default.js

--- tests/configs/modules/calendar/default.js
+++ tests/configs/modules/calendar/default.js
@@ -1,7 +1,4 @@
-/* Magic Mirror Test config default calendar
- *
- * By Rodrigo Ramírez Norambuena https://rodrigoramirez.com
- * MIT Licensed.
+/* Smart Mirror Test config default calendar
  */
 
 var config = {
