ef748f2a521d993e62dafecdb6b1dada6c7c0004
Shivam Shrivastava <19shivamshrivastava@gmail.com>
2018-03-20 13:01:29 +0530

    Update default.js

--- tests/configs/modules/newsfeed/default.js
+++ tests/configs/modules/newsfeed/default.js
@@ -1,7 +1,4 @@
-/* Magic Mirror Test config newsfeed module
- *
- * By Rodrigo Ramírez Norambuena https://rodrigoramirez.com
- * MIT Licensed.
+/* Smart Mirror Test config newsfeed module
  */
 
 var config = {
